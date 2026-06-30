089e97fc0201bd3d760388061c67637ee4bcf5a9
dellepianevalentina-blip <dellepiane.valentina@isanagustin.edu.ar>
2026-06-30 18:45:39 -0300

    Update image source for Laika in modal

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ const modalContentData = {
         subtitle: 'El Primer Ser Vivo en Órbita',
         html: `
             <div class="modal-img-container">
-                <img src="assets/laika.png" class="modal-img" alt="Laika en el Sputnik 2">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3zYre5MpsVeH4xR2NuLS7E6DOeojH0AhMVfIkJfT2WQ2alpZx_2kH6gk&s=10" class="modal-img" alt="Laika en el Sputnik 2">
                 <div class="modal-img-caption">Ilustración artística de Laika a bordo de la cabina presurizada del Sputnik 2.</div>
             </div>
             
